b0294fda5f9374a9c37edd86be6d66715a7b40d0
Toikid <183738110+Toikid23@users.noreply.github.com>
2024-11-10 17:46:56 +0100

    solution exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if (typeof f !== 'number') {
+    return "ERROR"
+  } else {
+    return Math.round((f - 32) * (5 / 9) * 10) / 10
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  if (typeof c !== 'number') {
+    return "ERROR"
+  } else {
+    return Math.round((c * (9 / 5) + 32) * 10 ) / 10
+  }
 };
 
 // Do not edit below this line
